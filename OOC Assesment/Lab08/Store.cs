5749c9e33100e4463e205ded5c08c95df2eadd1e
RIFAF RAHMAN <127125526+MathaThikNai@users.noreply.github.com>
2023-03-16 21:28:11 +0600

    Project Added

---
+++ OOC Assesment/Lab08/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08
+{
+    public class Store
+    {
+        public Store() { }
+
+        public List<Product> products = new List<Product>();
+        private Manager _manager { get; set; }
+        public SalesAssociate _saler { get; set; }
+
+        public void showStoreDetails()
+        {
+            Console.WriteLine($"Manager : {_manager._name}" +
+                $"SalesAssociate : {_saler._name}"+
+                $"Products : {products.Count}");
+        }
+    }
+}
