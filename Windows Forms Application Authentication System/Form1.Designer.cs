0660fb56292746418d895bdffb04b5d8b81f4489
MathaThikNai <sayedurrahman@iut-dhaka.edu>
2023-03-16 11:56:12 +0600

    Project added

---
+++ Windows Forms Application Authentication System/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace lab09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CreateOneLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(173, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameTextBox.Location = new System.Drawing.Point(386, 21);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(201, 35);
+            this.UserNameTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTextBox.Location = new System.Drawing.Point(386, 83);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(201, 43);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Location = new System.Drawing.Point(325, 213);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(109, 49);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Log in";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(175, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Don\'t have an account?";
+            // 
+            // CreateOneLinkLabel
+            // 
+            this.CreateOneLinkLabel.AutoSize = true;
+            this.CreateOneLinkLabel.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateOneLinkLabel.LinkColor = System.Drawing.Color.Red;
+            this.CreateOneLinkLabel.Location = new System.Drawing.Point(381, 149);
+            this.CreateOneLinkLabel.Name = "CreateOneLinkLabel";
+            this.CreateOneLinkLabel.Size = new System.Drawing.Size(88, 18);
+            this.CreateOneLinkLabel.TabIndex = 6;
+            this.CreateOneLinkLabel.TabStop = true;
+            this.CreateOneLinkLabel.Text = "Create one.";
+            this.CreateOneLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.CreateOneLinkLabel_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CreateOneLinkLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UserNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel CreateOneLinkLabel;
+    }
+}
+
---
+++ Windows Forms Application Authentication System/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab09
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 form1instance;
+        public string username;
+        public Form1()
+        {
+            InitializeComponent();
+            form1instance = this;
+        }
+
+        private void CreateOneLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            this.Hide();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string file = "X:\\Project\\SWE4202_210042118\\Windows Forms Application Authentication System\\username&password_list.txt";
+            bool flg1 = false;
+            if (File.Exists(file))
+            {
+                string[] lines = File.ReadAllLines(file);
+                for(int i = 0; i < lines.Length; i++)
+                {
+                    if (i % 3 == 1 && lines[i] == UserNameTextBox.Text)
+                    {
+                        if (lines[i + 1] == PasswordTextBox.Text) flg1 = true;
+                    }
+                }
+                if (flg1)
+                {
+                    username = UserNameTextBox.Text;
+                    Form3 f3 = new Form3();
+                    f3.Show();
+                    this.Hide();
+                }
+            }
+        }
+    }
+}
